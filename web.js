e90292d9647e26bfc517947ed402c00e1cbf435f
Ubuntu <ubuntu@ip-172-31-16-219.us-west-2.compute.internal>
2013-07-12 05:12:26 +0000

    Updated web.js again

--- web.js
+++ web.js
@@ -8,11 +8,11 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
-  //var buffer = fs.readFileSync'(index.html');
-  //var indexstring = buffer.buf.toString();
-  //response.send(indexstring);
-  var buffer = new buffer();
-  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")));
+  var buffer = fs.readFileSync'(index.html');
+  var indexstring = buffer.buf.toString();
+  response.send(indexstring);
+  //var buffer = new buffer();
+  //response.send(buffer.toString('utc 8', fs.readFileSync("index.html")));
 });
 
 var port = process.env.PORT || 5000;
